fc6ddf07fd6ac7dab63bf11b0824a72052454792
pezy <pezy.dev@gmail.com>
2017-07-22 16:57:47 +0800

    init the fork

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # imgproc
-Use Simple GUI to study image processing by Qt and OpenCV
+
+Fork from [QtOpenCV](https://github.com/dbzhang800/QtOpenCV)'s example: imageprocess.
+
+QtOpenCV is an amazing integration of OpenCV and Qt(cv::Mat <==> QImage). I highly recommend to star it.
+
+## Changes
+
+- Change enviroment to Visual Studio 2017 + Qt 5.9.1
+- Added Icon and Toolbar for MainWindow.
---
+++ imgproc/GeneratedFiles/ui_mainwindow.h
@@ -0,0 +1,184 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.9.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QDockWidget>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSplitter>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+#include "cvimagewidget.h"
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *action_Open;
+    QAction *action_Save;
+    QAction *actionSave_As;
+    QAction *actionRecent_Files;
+    QWidget *centralWidget;
+    QHBoxLayout *horizontalLayout;
+    QSplitter *splitter;
+    QtOcv::ImageWidget *originalView;
+    QtOcv::ImageWidget *processView;
+    QMenuBar *menuBar;
+    QMenu *menu_File;
+    QMenu *menuProcess;
+    QMenu *menu_Help;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+    QDockWidget *filterDockWidget;
+    QWidget *dockWidgetContents_3;
+    QVBoxLayout *verticalLayout;
+    QWidget *paramsWidget;
+    QVBoxLayout *verticalLayout_2;
+    QPushButton *setBaseBtn;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(600, 400);
+        QIcon icon;
+        icon.addFile(QStringLiteral(":/MainWindow/Resources/imgproc.png"), QSize(), QIcon::Normal, QIcon::Off);
+        MainWindow->setWindowIcon(icon);
+        action_Open = new QAction(MainWindow);
+        action_Open->setObjectName(QStringLiteral("action_Open"));
+        QIcon icon1;
+        icon1.addFile(QStringLiteral(":/MainWindow/Resources/open.png"), QSize(), QIcon::Normal, QIcon::Off);
+        action_Open->setIcon(icon1);
+        action_Save = new QAction(MainWindow);
+        action_Save->setObjectName(QStringLiteral("action_Save"));
+        QIcon icon2;
+        icon2.addFile(QStringLiteral(":/MainWindow/Resources/save.png"), QSize(), QIcon::Normal, QIcon::Off);
+        action_Save->setIcon(icon2);
+        actionSave_As = new QAction(MainWindow);
+        actionSave_As->setObjectName(QStringLiteral("actionSave_As"));
+        QIcon icon3;
+        icon3.addFile(QStringLiteral(":/MainWindow/Resources/saveas.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionSave_As->setIcon(icon3);
+        actionRecent_Files = new QAction(MainWindow);
+        actionRecent_Files->setObjectName(QStringLiteral("actionRecent_Files"));
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        horizontalLayout = new QHBoxLayout(centralWidget);
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        splitter = new QSplitter(centralWidget);
+        splitter->setObjectName(QStringLiteral("splitter"));
+        splitter->setOrientation(Qt::Horizontal);
+        originalView = new QtOcv::ImageWidget(splitter);
+        originalView->setObjectName(QStringLiteral("originalView"));
+        splitter->addWidget(originalView);
+        processView = new QtOcv::ImageWidget(splitter);
+        processView->setObjectName(QStringLiteral("processView"));
+        splitter->addWidget(processView);
+
+        horizontalLayout->addWidget(splitter);
+
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 600, 21));
+        menu_File = new QMenu(menuBar);
+        menu_File->setObjectName(QStringLiteral("menu_File"));
+        menuProcess = new QMenu(menuBar);
+        menuProcess->setObjectName(QStringLiteral("menuProcess"));
+        menu_Help = new QMenu(menuBar);
+        menu_Help->setObjectName(QStringLiteral("menu_Help"));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        mainToolBar->setIconSize(QSize(16, 16));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+        filterDockWidget = new QDockWidget(MainWindow);
+        filterDockWidget->setObjectName(QStringLiteral("filterDockWidget"));
+        filterDockWidget->setAllowedAreas(Qt::LeftDockWidgetArea|Qt::RightDockWidgetArea);
+        dockWidgetContents_3 = new QWidget();
+        dockWidgetContents_3->setObjectName(QStringLiteral("dockWidgetContents_3"));
+        verticalLayout = new QVBoxLayout(dockWidgetContents_3);
+        verticalLayout->setSpacing(6);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        paramsWidget = new QWidget(dockWidgetContents_3);
+        paramsWidget->setObjectName(QStringLiteral("paramsWidget"));
+        verticalLayout_2 = new QVBoxLayout(paramsWidget);
+        verticalLayout_2->setSpacing(6);
+        verticalLayout_2->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_2->setObjectName(QStringLiteral("verticalLayout_2"));
+
+        verticalLayout->addWidget(paramsWidget);
+
+        setBaseBtn = new QPushButton(dockWidgetContents_3);
+        setBaseBtn->setObjectName(QStringLiteral("setBaseBtn"));
+
+        verticalLayout->addWidget(setBaseBtn);
+
+        filterDockWidget->setWidget(dockWidgetContents_3);
+        MainWindow->addDockWidget(static_cast<Qt::DockWidgetArea>(1), filterDockWidget);
+
+        menuBar->addAction(menu_File->menuAction());
+        menuBar->addAction(menuProcess->menuAction());
+        menuBar->addAction(menu_Help->menuAction());
+        menu_File->addAction(action_Open);
+        menu_File->addAction(actionRecent_Files);
+        menu_File->addSeparator();
+        menu_File->addAction(action_Save);
+        menu_File->addAction(actionSave_As);
+        mainToolBar->addAction(action_Open);
+        mainToolBar->addAction(action_Save);
+        mainToolBar->addAction(actionSave_As);
+        mainToolBar->addSeparator();
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "imgproc", Q_NULLPTR));
+        action_Open->setText(QApplication::translate("MainWindow", "&Open...", Q_NULLPTR));
+        action_Save->setText(QApplication::translate("MainWindow", "&Save", Q_NULLPTR));
+        actionSave_As->setText(QApplication::translate("MainWindow", "Save &As...", Q_NULLPTR));
+        actionRecent_Files->setText(QApplication::translate("MainWindow", "Recent Files", Q_NULLPTR));
+        menu_File->setTitle(QApplication::translate("MainWindow", "&File", Q_NULLPTR));
+        menuProcess->setTitle(QApplication::translate("MainWindow", "&Process", Q_NULLPTR));
+        menu_Help->setTitle(QApplication::translate("MainWindow", "&Help", Q_NULLPTR));
+        filterDockWidget->setWindowTitle(QApplication::translate("MainWindow", "Image Filter", Q_NULLPTR));
+        setBaseBtn->setText(QApplication::translate("MainWindow", "SetBase", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
